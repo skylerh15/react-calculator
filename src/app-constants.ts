7ca460185e34126bbd066777b0c5b4f451f52d3c
Skyler Hiscock <skylerhiscock@gmail.com>
2019-08-02 11:28:33 -0500

    reorder buttons

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,9 +1,8 @@
 import { CalcValues } from './enums';
 
 export const ButtonOrder = [
-    [CalcValues.Divide, CalcValues.Multiply, CalcValues.Subtract, CalcValues.Add],
-    [CalcValues.Seven, CalcValues.Eight, CalcValues.Nine],
-    [CalcValues.Four, CalcValues.Five, CalcValues.Six],
-    [CalcValues.One, CalcValues.Two, CalcValues.Three],
-    [CalcValues.Zero, CalcValues.Decimal, CalcValues.Enter]
+    [CalcValues.Seven, CalcValues.Eight, CalcValues.Nine, CalcValues.Divide],
+    [CalcValues.Four, CalcValues.Five, CalcValues.Six, CalcValues.Multiply],
+    [CalcValues.One, CalcValues.Two, CalcValues.Three, CalcValues.Subtract],
+    [CalcValues.Zero, CalcValues.Decimal, CalcValues.Enter, CalcValues.Add]
 ];
